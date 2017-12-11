c3ce75c44b7dbed3471d19cd9b61f44646e2f382
sportarty <sportarty@beast.beast.com>
2017-12-11 16:24:29 +0300

    #1 реализовано идеома piml для класса endpoint

---
+++ inc/endpoint_traits.h
@@ -0,0 +1,12 @@
+#ifndef _ENDPOINT_TRAITS_H_
+#define _ENDPOINT_TRAITS_H_
+
+//posix
+#include <sys/socket.h>
+
+//stl
+#include <memory>
+#include <string>
+#include <iostream>
+
+#endif //_ENDPOINT_TRAITS_H_
